7ec5416b257717403b666aa7a319b50d6d48ffc7
Andrew Morris <andrewmorris@mailbox.org>
2024-11-04 11:06:18 +0100

    spellcheck

--- R/boot_conf.R
+++ R/boot_conf.R
@@ -1,10 +1,10 @@
-#' Calculates the 95% confidence interval using boostrapping
+#' Calculates the 95% confidence interval using bootstrapping
 #' 
 #' Internal function. Not intended for users. This function can return the 95%
 #' confidence intervals for any statistic so long as the function passed to `f`
 #' returns a single value of that statistic.
 #'
-#' @param df a data.frame containing the clumnes phs, age, and status
+#' @param df a data.frame containing the columns phs, age, and status
 #' @param bootstrap_iterations number of bootstrap iterations to run.
 #' @param f the name of a function to perform bootstrapping on. 
 #' @param ... arguments to be passed on to `f()`.
